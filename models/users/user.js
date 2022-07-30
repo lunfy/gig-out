83ae356afd5730e5d44cd2fd82cd2f76d1a4eea9
Lutfi Aman <lunfy@Lutfi-MBP.local>
2022-07-30 16:05:40 +0800

    feat(models/users/user.js): userSchema

---
+++ models/users/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        requried: true
+    },
+    accountCreated: {
+        type: Date,
+        required: true
+    },
+    lastActiveAt: {
+        type: Date
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
